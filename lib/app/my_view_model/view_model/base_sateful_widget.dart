9919e3fc20b85d6e5cb2548ecb6affba748a0cd9
Thiago Caiado <thiago.caiado@hotmail.com>
2023-06-03 17:00:06 -0300

    add dependencies

--- lib/app/my_view_model/view_model/base_sateful_widget.dart
+++ lib/app/my_view_model/view_model/base_sateful_widget.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
-abstract class StateWidget<E extends StatefulWidget> extends State<E> {
+class StateWidget<E extends StatefulWidget> extends State<E> {
+
+  late final Widget child;
+
+  Function? _elementRebuild;
+
+  StateWidget({required this.child});
 
   @override
   void initState() {
     super.initState();
+  }
+
+  void rebuild() {
+    if (_elementRebuild != null) {
+      _elementRebuild!.call();
+    }
+  }
+
+  @override
+  Widget build(covariant Element context) {
+    _elementRebuild = context.markNeedsBuild;
+    return child;
   }
 }
